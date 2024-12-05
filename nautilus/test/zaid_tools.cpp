b043a8810d34f89db3fafc5d609a22c3569d6589
Brendan K. Krueger <bkkrueger@lanl.gov>
2024-12-05 11:49:11 -0700

    Add zaid_tools.hpp and its tests

---
+++ nautilus/test/zaid_tools.cpp
@@ -0,0 +1,112 @@
+#include "zaid_tools.hpp"
+
+#ifndef CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_FAST_COMPILE
+#endif
+#include <catch2/catch_test_macros.hpp>
+
+#include <algorithm>
+#include <iostream>
+
+TEST_CASE("chemsym to zaid", "[zaid_tools]")
+{
+    using nautilus::chemsym_to_zaid;
+    int const zaid_g = chemsym_to_zaid("g");
+    CHECK(zaid_g == 0);
+    int const zaid_n = chemsym_to_zaid("n");
+    CHECK(zaid_n == 1);
+    int const zaid_p = chemsym_to_zaid("p");
+    CHECK(zaid_p == 1001);
+    int const zaid_h1 = chemsym_to_zaid("h1");
+    CHECK(zaid_h1 == 1001);
+    int const zaid_d = chemsym_to_zaid("d");
+    CHECK(zaid_d == 1002);
+    int const zaid_h2 = chemsym_to_zaid("h2");
+    CHECK(zaid_h2 == 1002);
+    int const zaid_t = chemsym_to_zaid("t");
+    CHECK(zaid_t == 1003);
+    int const zaid_h3 = chemsym_to_zaid("h3");
+    CHECK(zaid_h3 == 1003);
+    int const zaid_he3 = chemsym_to_zaid("he3");
+    CHECK(zaid_he3 == 2003);
+    int const zaid_a = chemsym_to_zaid("a");
+    CHECK(zaid_a == 2004);
+    int const zaid_he4 = chemsym_to_zaid("he4");
+    CHECK(zaid_he4 == 2004);
+    int const zaid_li6 = chemsym_to_zaid("li6");
+    CHECK(zaid_li6 == 3006);
+    int const zaid_li7 = chemsym_to_zaid("li7");
+    CHECK(zaid_li7 == 3007);
+    int const zaid_c12 = chemsym_to_zaid("c12");
+    CHECK(zaid_c12 == 6012);
+    int const zaid_ni56 = chemsym_to_zaid("ni56");
+    CHECK(zaid_ni56 == 28056);
+    int const zaid_es253 = chemsym_to_zaid("es253");
+    CHECK(zaid_es253 == 99253);
+    int const zaid_og294 = chemsym_to_zaid("og294");
+    CHECK(zaid_og294 == 118294);
+}
+
+TEST_CASE("isotope zaids from reaction zaid", "[zaid_tools]")
+{
+    using nautilus::get_isotope_zaids;
+    using nautilus::get_product_zaids;
+    using nautilus::get_reactant_zaids;
+
+    // special cases
+    std::string const dtna{"d+t->n+a"};
+    std::cout << dtna << std::endl;
+    auto dtna_reactants = get_reactant_zaids(dtna);
+    CHECK(dtna_reactants.size() == 2);
+    CHECK(dtna_reactants[0] == 1002);
+    CHECK(dtna_reactants[1] == 1003);
+    auto dtna_products = get_product_zaids(dtna);
+    CHECK(dtna_products.size() == 2);
+    CHECK(dtna_products[0] == 1);
+    CHECK(dtna_products[1] == 2004);
+    auto dtna_isotopes = get_isotope_zaids(dtna);
+    std::sort(dtna_isotopes.begin(), dtna_isotopes.end());
+    CHECK(dtna_isotopes.size() == 4);
+    CHECK(dtna_isotopes[0] == 1);
+    CHECK(dtna_isotopes[1] == 1002);
+    CHECK(dtna_isotopes[2] == 1003);
+    CHECK(dtna_isotopes[3] == 2004);
+
+    // three reactants
+    std::string const nb10{"n+b10->a+li7+g"};
+    std::cout << nb10 << std::endl;
+    auto nb10_reactants = get_reactant_zaids(nb10);
+    CHECK(nb10_reactants.size() == 2);
+    CHECK(nb10_reactants[0] == 1);
+    CHECK(nb10_reactants[1] == 5010);
+    auto nb10_products = get_product_zaids(nb10);
+    CHECK(nb10_products.size() == 3);
+    CHECK(nb10_products[0] == 2004);
+    CHECK(nb10_products[1] == 3007);
+    CHECK(nb10_products[2] == 0);
+    auto nb10_isotopes = get_isotope_zaids(nb10);
+    std::sort(nb10_isotopes.begin(), nb10_isotopes.end());
+    CHECK(nb10_isotopes.size() == 5);
+    CHECK(nb10_isotopes[0] == 0);
+    CHECK(nb10_isotopes[1] == 1);
+    CHECK(nb10_isotopes[2] == 2004);
+    CHECK(nb10_isotopes[3] == 3007);
+    CHECK(nb10_isotopes[4] == 5010);
+
+    // multiplicity > 1 in both reactants and products
+    std::string const tt2na{"t+t->2n+a"};
+    std::cout << tt2na << std::endl;
+    auto tt2na_reactants = get_reactant_zaids(tt2na);
+    CHECK(tt2na_reactants.size() == 1);
+    CHECK(tt2na_reactants[0] == 1003);
+    auto tt2na_products = get_product_zaids(tt2na);
+    CHECK(tt2na_products.size() == 2);
+    CHECK(tt2na_products[0] == 1);
+    CHECK(tt2na_products[1] == 2004);
+    auto tt2na_isotopes = get_isotope_zaids(tt2na);
+    std::sort(tt2na_isotopes.begin(), tt2na_isotopes.end());
+    CHECK(tt2na_isotopes.size() == 3);
+    CHECK(tt2na_isotopes[0] == 1);
+    CHECK(tt2na_isotopes[1] == 1003);
+    CHECK(tt2na_isotopes[2] == 2004);
+}
---
+++ nautilus/zaid_tools.hpp
@@ -0,0 +1,144 @@
+#ifndef NAUTILUS_ZAIDTOOLS_HPP
+#define NAUTILUS_ZAIDTOOLS_HPP
+
+#include <cassert>
+#include <string>
+#include <vector>
+
+namespace nautilus {
+
+// TODO: There should be a corresponding zaid-to-chemsym conversion routine
+inline int chemsym_to_zaid(std::string const & chemsym)
+{
+    if (chemsym == "g") {
+        // special case: gammas / photons
+        return 0;
+    } else if (chemsym == "n") {
+        // special case: neutrons
+        return 1;
+    } else if (chemsym == "p") {
+        // special case: protons / H-1
+        return 1001;
+    } else if (chemsym == "d") {
+        // special case: deuterons / H-2
+        return 1002;
+    } else if (chemsym == "t") {
+        // special case: tritons / H-3
+        return 1003;
+    } else if (chemsym == "a") {
+        // special case: alpha / He-4
+        return 2004;
+    } else {
+        // split into element symbol and atomic mass number
+        auto const pos = chemsym.find_first_of("0123456789");
+        auto element_symbol = chemsym.substr(0, pos);
+        auto atomic_mass_number = chemsym.substr(pos);
+        // look-up table to convert element symbol to atomic number
+        std::vector<std::string> const elements{
+            "h",  "he", "li", "be", "b",  "c",  "n",  "o",  "f",  "ne", "na", "mg", "al", "si",
+            "p",  "s",  "cl", "ar", "k",  "ca", "sc", "ti", "v",  "cr", "mn", "fe", "co", "ni",
+            "cu", "zn", "ga", "ge", "as", "se", "br", "kr", "rb", "sr", "y",  "zr", "nb", "mo",
+            "tc", "ru", "rh", "pd", "ag", "cd", "in", "sn", "sb", "te", "i",  "xe", "cs", "ba",
+            "la", "ce", "pr", "nd", "pm", "sm", "eu", "gd", "tb", "dy", "ho", "er", "tm", "yb",
+            "lu", "hf", "ta", "w",  "te", "os", "ir", "pt", "au", "hg", "tl", "pb", "bi", "po",
+            "at", "rn", "fr", "ra", "ac", "th", "pa", "u",  "np", "pu", "am", "cm", "bk", "cf",
+            "es", "fm", "md", "no", "lr", "rf", "db", "sg", "bh", "hs", "mt", "ds", "rg", "cn",
+            "nh", "fl", "mc", "lv", "ts", "og"};
+        int Z = -1;
+        for (std::size_t n{0}; n < elements.size(); ++n) {
+            if (elements[n] == element_symbol) {
+                Z = static_cast<int>(n) + 1;
+                break;
+            }
+        }
+        // -- Z < 0 is an error: didn't find the result
+        // -- all Z == 0 cases are special cases handled previously
+        assert(Z > 0);
+        // For historical reasons, Americium has special rules in the NDI format.  We don't yet
+        // handle those rules correctly, so we simply forbid Americium at this time.  This will be
+        // addressed in the future, but is low priority since we have no reactions that involve
+        // Americium as either reactant or product.
+        assert(Z != 95);
+        // convert string to integer for atomic mass number
+        int N = std::stoi(atomic_mass_number);
+        // return zaid
+        return 1000 * Z + N;
+    }
+}
+
+inline std::vector<std::string> half_reaction_zaid_to_chemsym_list(std::string const & half_rzaid)
+{
+    // notes on std::string::npos:
+    // -- has a value of -1, but is unsigned so wraps around to largest value (see
+    //    https://en.cppreference.com/w/cpp/string/basic_string/npos)
+    // -- original.substr(0, npos) copies all of original
+    // -- npos + 1 = 0, due to unsigned wrapround, so original.substr(npos + 1) copies all of
+    //    original (which is nothing if original is the empty string)
+    std::string remainder = half_rzaid;
+    std::vector<std::string> chemsym_list;
+    std::string::size_type pos;
+    do {
+        // get next chemsym
+        pos = remainder.find('+');
+        auto iso_str = remainder.substr(0, pos); // npos = -1 (unsigned wraparound), copies all
+        // strip leading multiplicity
+        iso_str = iso_str.substr(iso_str.find_first_not_of("0123456789"));
+        // append to list
+        chemsym_list.push_back(iso_str);
+        // cycle
+        remainder = remainder.substr(pos + 1); // npos = -1 (unsigned wraparound), npos + 1 == 0
+    } while (pos != std::string::npos);
+    return chemsym_list;
+}
+
+inline void append_zaids(std::vector<int> & zaid_list, std::string const & half_rzaid)
+{
+    auto const chemsym_list = half_reaction_zaid_to_chemsym_list(half_rzaid);
+    auto append_if_unique = [&zaid_list](int const zaid) {
+        bool unique = true;
+        for (std::size_t n{0}; n < zaid_list.size(); ++n) {
+            if (zaid_list[n] == zaid) {
+                unique = false;
+                break;
+            }
+        }
+        if (unique) {
+            zaid_list.push_back(zaid);
+        }
+    };
+    for (auto & chemsym : chemsym_list) {
+        append_if_unique(chemsym_to_zaid(chemsym));
+    }
+}
+
+inline auto get_reactant_zaids(std::string const & rzaid)
+{
+    auto split_pos = rzaid.find("->");
+    assert(split_pos != std::string::npos);
+    std::vector<int> zaid_list;
+    append_zaids(zaid_list, rzaid.substr(0, split_pos));
+    return zaid_list;
+}
+
+inline auto get_product_zaids(std::string const & rzaid)
+{
+    auto split_pos = rzaid.find("->");
+    assert(split_pos != std::string::npos);
+    std::vector<int> zaid_list;
+    append_zaids(zaid_list, rzaid.substr(split_pos + 2));
+    return zaid_list;
+}
+
+inline auto get_isotope_zaids(std::string const & rzaid)
+{
+    auto split_pos = rzaid.find("->");
+    assert(split_pos != std::string::npos);
+    std::vector<int> zaid_list;
+    append_zaids(zaid_list, rzaid.substr(0, split_pos));
+    append_zaids(zaid_list, rzaid.substr(split_pos + 2));
+    return zaid_list;
+}
+
+} // namespace nautilus
+
+#endif // NAUTILUS_ZAIDTOOLS_HPP
