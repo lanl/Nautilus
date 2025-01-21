a85dcc809241b14b9926af91dc75f1ca55251f19
Richard Berger <rberger@lanl.gov>
2025-01-21 13:02:56 -0700

    spack: update package.py

--- spack-repo/packages/nautilus/package.py
+++ spack-repo/packages/nautilus/package.py
@@ -1,9 +1,8 @@
-# Copyright 2013-2022 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-from spack import *
+from spack.package import *
 
 class Nautilus(CMakePackage, CudaPackage, ROCmPackage):
     """Nautilus library."""
