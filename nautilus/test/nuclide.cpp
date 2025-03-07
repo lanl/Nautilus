53059506d62abe15e392d2ab89ad41561a91f0e3
Brendan K. Krueger <bkkrueger@lanl.gov>
2025-03-07 11:03:44 -0700

    Update include

--- nautilus/test/nuclide.cpp
+++ nautilus/test/nuclide.cpp
@@ -1,4 +1,4 @@
-#include "isotope.hpp"
+#include "nuclide.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
--- nautilus/test/nuclide_kokkos.cpp
+++ nautilus/test/nuclide_kokkos.cpp
@@ -1,4 +1,4 @@
-#include "isotope.hpp"
+#include "nuclide.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
