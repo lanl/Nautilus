7c475e96ed0bb158fc89a027396743ca54c786c1
Brendan K. Krueger <bkkrueger@lanl.gov>
2025-03-27 18:02:07 -0600

    work in progress

--- nautilus/tag/format_standard_name.hpp
+++ nautilus/tag/format_standard_name.hpp
@@ -1,25 +1,18 @@
 #ifndef NAUTILUS_FORMAT_STANDARD_HPP
 #define NAUTILUS_FORMAT_STANDARD_HPP
 
+#include <names.hpp>
 #include "pantag.hpp"
 
-#include "ports-of-call/portability.hpp"
+#include "ports-of-call/portability.hpp" // TODO: Add PORTABLE_FUNCTION annotations
 
 #include <cassert>
+// TODO: We don't want std::string, but something that runs on GPUs (std::string_view?)
 #include <string>
 
+// TODO: Should some/all of these be constexpr?
+
 namespace nautilus::tag {
-
-// TODO: IUPAC standard long names versus national standard long names
-//      Z       IUPAC       American        Canadian        British
-//      13      aluminium   aluminum        aluminum
-//      16      sulfur                                      sulphur
-//      55      caesium     cesium
-
-// TODO: For particles, should I add LaTeX output?  Then I can fix the "quirks" of things that
-//       aren't currently present in Unicode, and I can also provide the alternate convention
-//       (where the overline extends over the sign).  This may be added to the list of, "We could
-//       add this, but we'll wait for someone to ask for it."
 
 // ================================================================================================
 
@@ -31,7 +24,7 @@ std::string to_short_iupac_name(const Pantag tag)
 {
     assert(tag.is_nuclide() && tag.is_standard());
     auto ci_str = [](const char c, const auto n) { return std::to_string(c) + std::to_string(n); };
-    const std::string name = get_element(tag.get_atomic_number()).symbol;
+    const std::string name = Nuclides::get_symbol(tag.get_atomic_number());
     const std::string isotope = ci_str('-', tag.get_atomic_mass_number());
     const std::string suffix =
         (tag.is_ground() ? "" : ci_str(tag.has_metastable_index() ? 'm' : 'e', tag.get_index()));
@@ -44,11 +37,11 @@ std::string to_short_iupac_name(const Pantag tag)
 // -- 12 characters for element name
 // --  4 characters for atomic mass number ("-###")
 // --  4 characters for excitation/metastable suffix ("[em]###")
-std::string to_long_iupac_name(const Pantag tag)
+std::string to_long_iupac_name(const Pantag tag, const standards_flag_t standard)
 {
     assert(tag.is_nuclide() && tag.is_standard());
     auto ci_str = [](const char c, const auto n) { return std::to_string(c) + std::to_string(n); };
-    const std::string name = get_element(tag.get_atomic_number()).name;
+    const std::string name = Nuclides::get_name(tag.get_atomic_number(), standard);
     const std::string isotope = ci_str('-', tag.get_atomic_mass_number());
     const std::string suffix =
         (tag.is_ground() ? "" : ci_str(tag.has_metastable_index() ? 'm' : 'e', tag.get_index()));
@@ -57,115 +50,26 @@ std::string to_long_iupac_name(const Pantag tag)
 
 // ================================================================================================
 
-// Warning: These use UTF-8 code points, so the string may not behave as expected (for example,
-//          size may report a longer result than the actual number of printed characters).  Using
-//          something like wstring instead leads to different complications, so this was the
-//          compromise chosen by Nautilus.
-// Warning: Subscript mu does not appear to exist in Unicode at this time, so I had to substitute a
-//          subscript lowercase m.
-// Warning: Subscript uppercase L and S don't appear to exist in Unicode at this time, and they
-//          would be more standard for the long and short kaons.  Instead I had to substitute
-//          lowercase subscripts.
-// Warning: Unicode does not provide the ability to stack a subscript and a superscript vertically,
-//          but must place them side-by-side.  For the long and short kaons, we place the
-//          superscript first and the subscript after.
 std::string to_short_pdg_name(const Pantag tag)
 {
-    using Pantag::Particle;
     assert(tag.is_particle() && tag.is_standard());
-    // clang-format off
-    switch (tag.data()) {
-    case(photon):                       return "\u03B3";             break;
-    case(electron):                     return "e\u207B";            break;
-    case(positron):                     return "e\u207A";            break;
-    case(electron_neutrino):            return "\u03BD\u2091";       break;
-    case(electron_antineutrino):        return "\u03Bd\u0304\u2091"; break;
-    case(muon):                         return "\u03BC\u207B";       break;
-    case(antimuon):                     return "\u03BC\u0304\u207A"; break;
-    case(muon_neutrino):                return "\u03BD\u2098";       break;
-    case(muon_antineutrino):            return "\u03BD\u0304\u2098"; break;
-    case(neutral_pion):                 return "\u03C0\u2070";       break;
-    case(positive_pion):                return "\u03C0\u207A";       break;
-    case(negative_pion):                return "\u03C0\u207B";       break;
-    case(short_kaon):                   return "K\u2070\u209B";      break;
-    case(long_kaon):                    return "K\u2070\u2097";      break;
-    case(positive_kaon):                return "K\u207A";            break;
-    case(negative_kaon):                return "K\u207B";            break;
-    case(neutron):                      return "n";                  break;
-    case(antineutron):                  return "n\u0304";            break;
-    case(proton):                       return "p";                  break;
-    case(antiproton):                   return "p\u0304";            break;
-    case(neutral_lambda_baryon):        return "\u039B\u2070";       break;
-    case(neutral_lambda_antibaryon):    return "\u039B\u0304\u2070"; break;
-    case(positive_sigma_baryon):        return "\u03A3\u207A";       break;
-    case(negative_sigma_antibaryon):    return "\u03A3\u0304\u207B"; break;
-    case(negative_sigma_baryon):        return "\u03A3\u207B";       break;
-    case(positive_sigma_antibaryon):    return "\u03A3\u0304\u207A"; break;
-    case(neutral_xi_baryon):            return "\u039E\u2070";       break;
-    case(neutral_xi_antibaryon):        return "\u039E\u0304\u2070"; break;
-    case(negative_xi_baryon):           return "\u039E\u207B";       break;
-    case(positive_xi_antibaryon):       return "\u039E\u0304\u207A"; break;
-    case(negative_omega_baryon):        return "\u03A9\u207B";       break;
-    case(positive_omega_antibaryon):    return "\u03A9\u0304\u207A"; break;
-    case default: assert(false);
-    }
-    // clang-format on
+    return Particles::get_symbol();
 }
 
 // ================================================================================================
 
-std::string to_long_pdg_name(const Pantag tag)
+std::string to_long_pdg_name(const Pantag tag, const standards_flag_t standard)
 {
-    using Pantag::Particle;
     assert(tag.is_particle() && tag.is_standard());
-    // clang-format off
-    switch (tag.data()) {
-    case(photon):                       return "photon";                    break;
-    case(electron):                     return "electron";                  break;
-    case(positron):                     return "positron";                  break;
-    case(electron_neutrino):            return "electron neutrino";         break;
-    case(electron_antineutrino):        return "electron antineutrino";     break;
-    case(muon):                         return "muon";                      break;
-    case(antimuon):                     return "antimuon";                  break;
-    case(muon_neutrino):                return "muon neutrino";             break;
-    case(muon_antineutrino):            return "muon antineutrino";         break;
-    case(neutral_pion):                 return "neutral pion";              break;
-    case(positive_pion):                return "positive pion";             break;
-    case(negative_pion):                return "negative pion";             break;
-    case(short_kaon):                   return "short kaon";                break;
-    case(long_kaon):                    return "long kaon";                 break;
-    case(positive_kaon):                return "positive kaon";             break;
-    case(negative_kaon):                return "negative kaon";             break;
-    case(neutron):                      return "neutron";                   break;
-    case(antineutron):                  return "antineutron";               break;
-    case(proton):                       return "proton";                    break;
-    case(antiproton):                   return "antiproton";                break;
-    case(neutral_lambda_baryon):        return "neutral lambda baryon";     break;
-    case(neutral_lambda_antibaryon):    return "neutral lambda antibaryon"; break;
-    case(positive_sigma_baryon):        return "positive sigma baryon";     break;
-    case(negative_sigma_antibaryon):    return "negative sigma antibaryon"; break;
-    case(negative_sigma_baryon):        return "negative sigma baryon";     break;
-    case(positive_sigma_antibaryon):    return "positive sigma antibaryon"; break;
-    case(neutral_xi_baryon):            return "neutral xi baryon";         break;
-    case(neutral_xi_antibaryon):        return "neutral xi antibaryon";     break;
-    case(negative_xi_baryon):           return "negative xi baryon";        break;
-    case(positive_xi_antibaryon):       return "positive xi antibaryon";    break;
-    case(negative_omega_baryon):        return "negative omega baryon";     break;
-    case(positive_omega_antibaryon):    return "positive omega antibaryon"; break;
-    case default: assert(false);
-    }
-    // clang-format on
+    return Particles::get_name(standard);
 }
 
 // ================================================================================================
 
-// TODO: std::string isn't constexpr until... when?
-// TODO: std::string doesn't work on GPUs, right?  Is there a different data type I should use
-//       instead?  Or should this format be explicitly excluded from running on a GPU?  What about
-//       if someone wants to do some debugging of a GPU kernel and print out a tag?
-//    -- That may depend on how much of the existing string manipulation framework I would have to
-//       reimplement to make something that works on GPUs.
-//    -- If I take out std::string, make sure to delete the include as well.
+// For symbols:
+// -- nuclides only have a single standard
+// -- the symbols for the "alternate" particle convention cannot all be represented in Unicode, so
+//    you always get the PDG symbol
 std::string to_short_standard_name(const Pantag tag)
 {
     if (tag.is_nuclide()) {
@@ -177,19 +81,25 @@ std::string to_short_standard_name(const Pantag tag)
 
 // ================================================================================================
 
-// TODO: std::string isn't constexpr until... when?
-// TODO: std::string doesn't work on GPUs, right?  Is there a different data type I should use
-//       instead?  Or should this format be explicitly excluded from running on a GPU?  What about
-//       if someone wants to do some debugging of a GPU kernel and print out a tag?
-//    -- That may depend on how much of the existing string manipulation framework I would have to
-//       reimplement to make something that works on GPUs.
-//    -- If I take out std::string, make sure to delete the include as well.
-std::string to_long_standard_name(const Pantag tag)
+// TODO: Need to be able to specify a standard.  At this point in Nautilus, you can operate on tags
+//       without caring if you have a nuclide or a particle.  So how do we specify the standards?
+//    -- Possibility: Pass two standards flags.  If I switch to enums instead of std::size_t (not
+//       sure if that's a good or bad idea), then I could have four versions and rely on dispatch:
+//       both standards default, default for nuclides and non-default for particles, non-default
+//       for nuclides and default for particles, non-default for both.
+//    -- Possibility: Pack the standards flags.  This involves shifting one or both flags so that
+//       they don't overlap, and then the user can pass in something like
+//       "Nuclides::Standard::American | Particles::Standard::PDG".
+//    -- Possibility: Create translation routines: one to combine a Nuclides flag and a Particles
+//       flag, one to extract the Nuclides flag, and one to extract the Particles flag.  Something
+//       like "flag = standards(Nuclides::Standard::American, Particles::Standard::PDG)",
+//       "nuclide_standard(flag)", and "particle_standard(flag)"?
+std::string to_long_standard_name(const Pantag tag, const standards_flag_t standards)
 {
     if (tag.is_nuclide()) {
-        return to_long_iupac_name(tag);
+        return to_long_iupac_name(tag, nuclide_standard(standards));
     } else {
-        return to_long_pdg_name(tag);
+        return to_long_pdg_name(tag, particle_standard(standards));
     }
 }
 
