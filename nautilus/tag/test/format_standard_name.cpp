7ed598bab23929bd52d10e63a2cf8512c3d94b2d
Brendan K. Krueger <bkkrueger@lanl.gov>
2025-08-26 15:22:52 -0600

    more tests

--- nautilus/tag/test/format_standard_name.cpp
+++ nautilus/tag/test/format_standard_name.cpp
@@ -33,11 +33,11 @@ TEST_CASE("Format: Standard Name", "[tag][format][standard name]")
     // -- American
     // -- British
     // -- Canadian
-    CHECK(to_standard_name(Pantag(/*TODO*/)) == "oganesson-294");
-    CHECK(to_standard_name(Pantag(/*TODO*/), /*IUPAC*/) == "oganesson-294");
-    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:American*/) == "oganesson-294");
-    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:British*/) == "oganesson-294");
-    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:Canadian*/) == "oganesson-294");
+    CHECK(to_standard_name(Pantag(/*TODO*/)) == "nitrogen-14");
+    CHECK(to_standard_name(Pantag(/*TODO*/), /*IUPAC*/) == "nitrogen-14");
+    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:American*/) == "nitrogen-14");
+    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:British*/) == "nitrogen-14");
+    CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:Canadian*/) == "nitrogen-14");
 
     CHECK(to_standard_name(Pantag(/*TODO*/)) == "aluminium-27");
     CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:American*/) == "aluminum-27");
@@ -70,8 +70,15 @@ TEST_CASE("Format: Standard Name", "[tag][format][standard name]")
     CHECK(to_standard_name(Pantag(/*TODO*/), /*TODO:alternate*/) == "antiparticle of the neutral lambda baryon");
 
     // Distinction between hydrogen-1 (nuclide) and proton (particle)
-    CHECK(to_standard_name(Pantag(/*TODO*/)) == "photon");
+    CHECK(to_standard_name(Pantag(/*TODO*/)) == "proton");
     CHECK(to_standard_name(Pantag(/*TODO*/)) == "hydrogen-1");
+
+    // First and last (verify bounds of indexing)
+    CHECK(to_standard_name(/*TODO*/) == "hydrogen-1");
+    CHECK(to_standard_name(/*TODO*/) == "oganesson-294");
+    CHECK(to_standard_name(/*TODO*/) == "photon");
+    CHECK(to_standard_name(/*TODO*/) == "positive omega antibaryon");
+
 }
 
 // TODO: symbols
