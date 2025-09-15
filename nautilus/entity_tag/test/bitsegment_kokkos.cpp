53c71f4fbd8b6acb207f73a75ea7ffafb1edc292
Brendan K. Krueger <bkkrueger@lanl.gov>
2025-09-15 16:39:33 -0600

    Use GPU_CHECK in another file

--- nautilus/entity_tag/test/bitsegment_kokkos.cpp
+++ nautilus/entity_tag/test/bitsegment_kokkos.cpp
@@ -1,4 +1,5 @@
 #include "bitsegment.hpp"
+#include "nautilus/util/gpu_check.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -9,42 +10,22 @@
 TEST_CASE("BitSegment on GPUs", "[entity_tag][bitsegment][GPU]")
 {
     // The main tests are run on CPU.  These tests only demonstrate that the code runs on GPU.
-    constexpr size_t N{6};
     using HostSpace = Kokkos::HostSpace;
     using ExecSpace = Kokkos::DefaultExecutionSpace::memory_space;
-    Kokkos::View<int *, ExecSpace> results_gpu("results", N);
-    Kokkos::parallel_for(
-        N, KOKKOS_LAMBDA(int const n) {
-            results_gpu(n) = 0;
-            using T = int32_t;
-            using nautilus::entity_tag::BitSegment;
-            T num = 0x0000FFFF;
-            BitSegment<T, 16, 16> bs0;
-            if (bs0.mask() == static_cast<T>(0xFFFF0000)) {
-                results_gpu(n) += 1;
-            }
-            if (bs0.get(num) == 0x0000) {
-                results_gpu(n) += 2;
-            }
-            bs0.set(0xFFFF, num);
-            if (num == static_cast<T>(0xFFFFFFFF)) {
-                results_gpu(n) += 4;
-            }
-            BitSegment<T, 0, 16> bs1;
-            if (bs1.mask() == 0x0000FFFF) {
-                results_gpu(n) += 8;
-            }
-            if (bs1.get(num) == 0xFFFF) {
-                results_gpu(n) += 16;
-            }
-            bs1.set(0x0000, num);
-            if (num == static_cast<T>(0xFFFF0000)) {
-                results_gpu(n) += 32;
-            }
-            printf("[%s|%d] end, diff = %d\n", ExecSpace::name(), n, 63 - results_gpu(n));
-        });
-    auto results_cpu = Kokkos::create_mirror_view_and_copy(HostSpace(), results_gpu);
-    for (size_t n{0}; n < N; ++n) {
-        CHECK(results_cpu(n) == 63);
-    }
+    constexpr size_t N{6};
+    GPU_CHECK_INIT();
+    using T = int32_t;
+    using nautilus::entity_tag::BitSegment;
+    T num = 0x0000FFFF;
+    BitSegment<T, 16, 16> bs0;
+    GPU_CHECK(bs0.mask() == static_cast<T>(0xFFFF0000));
+    GPU_CHECK(bs0.get(num) == 0x0000);
+    bs0.set(0xFFFF, num);
+    GPU_CHECK(num == static_cast<T>(0xFFFFFFFF));
+    BitSegment<T, 0, 16> bs1;
+    GPU_CHECK(bs1.mask() == 0x0000FFFF);
+    GPU_CHECK(bs1.get(num) == 0xFFFF);
+    bs1.set(0x0000, num);
+    GPU_CHECK(num == static_cast<T>(0xFFFF0000));
+    GPU_CHECK_FINAL(6);
 }
--- nautilus/util/gpu_check.hpp
+++ nautilus/util/gpu_check.hpp
@@ -8,7 +8,11 @@
 
 // This machinery will allow up to 64 calls to GPU_CHECK between init and final.  It reserves the
 // names rg, rc, and ectr.  Calls to init and final MUST be properly paired, and only one such pair
-// can exist in a scope (consider using Catch2's SECTION to contain scopes).
+// can exist in a scope (consider using Catch2's SECTION to contain scopes).  You must define the
+// spaces and the number of kernels, for example:
+//    using HostSpace = Kokkos::HostSpace;
+//    using ExecSpace = Kokkos::DefaultExecutionSpace::memory_space;
+//    constexpr size_t N{6};
 constexpr uint64_t error_code(const uint64_t num_tests)
 {
     uint64_t code = 0;
