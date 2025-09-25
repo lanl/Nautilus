b6a26e60dd47cb4ad6b000b90d1c182658f64f30
Brendan K. Krueger <bkkrueger@lanl.gov>
2025-09-25 09:16:30 -0600

    stop using from_standard_name_or_symbol and use more specific versions (from_standard_name or from_standard_symbol)

--- nautilus/entity_tag/test/format_standard.cpp
+++ nautilus/entity_tag/test/format_standard.cpp
@@ -13,30 +13,29 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
 {
     using nautilus::entity_tag::EntityTag;
     using nautilus::entity_tag::from_standard_name;
-    using nautilus::entity_tag::from_standard_name_or_symbol;
     using nautilus::entity_tag::to_standard_name;
     using nautilus::entity_tag::names::Nuclides;
     using nautilus::entity_tag::names::Particles;
 
     // "Normal" names with optional cases
     constexpr EntityTag co59g(27, 59);
-    CHECK((from_standard_name_or_symbol("cobalt-59") == co59g));
-    CHECK((from_standard_name_or_symbol("cobalt-59g") == co59g));
+    CHECK((from_standard_name("cobalt-59") == co59g));
+    CHECK((from_standard_name("cobalt-59g") == co59g));
     CHECK(to_standard_name(co59g) == "cobalt-59");
 
     constexpr EntityTag ta180m1(73, 180, 1);
-    CHECK((from_standard_name_or_symbol("tantalum-180m") == ta180m1));
-    CHECK((from_standard_name_or_symbol("tantalum-180m1") == ta180m1));
+    CHECK((from_standard_name("tantalum-180m") == ta180m1));
+    CHECK((from_standard_name("tantalum-180m1") == ta180m1));
     CHECK(to_standard_name(ta180m1) == "tantalum-180m1");
 
     constexpr EntityTag k38m2(19, 38, 2);
-    CHECK((from_standard_name_or_symbol("potassium-38m2") == k38m2));
+    CHECK((from_standard_name("potassium-38m2") == k38m2));
     CHECK(to_standard_name(k38m2) == "potassium-38m2");
 
     // Alternate spellings
     constexpr EntityTag n14(7, 14);
     const std::string default_nitrogen = "nitrogen-14";
-    CHECK((from_standard_name_or_symbol(default_nitrogen) == n14));
+    CHECK((from_standard_name(default_nitrogen) == n14));
     CHECK(to_standard_name(n14) == default_nitrogen);
     CHECK(to_standard_name(n14, Nuclides::Standard::IUPAC) == default_nitrogen);
     CHECK(to_standard_name(n14, Nuclides::Standard::American) == default_nitrogen);
@@ -46,8 +45,8 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
     constexpr EntityTag al27(13, 27);
     const std::string default_aluminium = "aluminium-27";
     const std::string alternate_aluminium = "aluminum-27";
-    CHECK((from_standard_name_or_symbol(default_aluminium) == al27));
-    CHECK((from_standard_name_or_symbol(alternate_aluminium) == al27));
+    CHECK((from_standard_name(default_aluminium) == al27));
+    CHECK((from_standard_name(alternate_aluminium) == al27));
     CHECK(to_standard_name(al27) == default_aluminium);
     CHECK(to_standard_name(al27, Nuclides::Standard::IUPAC) == default_aluminium);
     CHECK(to_standard_name(al27, Nuclides::Standard::American) == alternate_aluminium);
@@ -57,8 +56,8 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
     constexpr EntityTag s32(16, 32);
     const std::string default_sulfur = "sulfur-32";
     const std::string alternate_sulfur = "sulphur-32";
-    CHECK((from_standard_name_or_symbol(default_sulfur) == s32));
-    CHECK((from_standard_name_or_symbol(alternate_sulfur) == s32));
+    CHECK((from_standard_name(default_sulfur) == s32));
+    CHECK((from_standard_name(alternate_sulfur) == s32));
     CHECK(to_standard_name(s32) == default_sulfur);
     CHECK(to_standard_name(s32, Nuclides::Standard::IUPAC) == default_sulfur);
     CHECK(to_standard_name(s32, Nuclides::Standard::American) == default_sulfur);
@@ -68,8 +67,8 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
     constexpr EntityTag cs55(55, 133);
     const std::string default_caesium = "caesium-133";
     const std::string alternate_caesium = "cesium-133";
-    CHECK((from_standard_name_or_symbol(default_caesium) == cs55));
-    CHECK((from_standard_name_or_symbol(alternate_caesium) == cs55));
+    CHECK((from_standard_name(default_caesium) == cs55));
+    CHECK((from_standard_name(alternate_caesium) == cs55));
     CHECK(to_standard_name(cs55) == default_caesium);
     CHECK(to_standard_name(cs55, Nuclides::Standard::IUPAC) == default_caesium);
     CHECK(to_standard_name(cs55, Nuclides::Standard::American) == alternate_caesium);
@@ -78,17 +77,17 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
 
     // Elementals
     constexpr EntityTag c_elemental(6);
-    CHECK((from_standard_name_or_symbol("elemental carbon") == c_elemental));
+    CHECK((from_standard_name("elemental carbon") == c_elemental));
     CHECK(to_standard_name(c_elemental) == "elemental carbon");
     constexpr EntityTag cs_elemental(55);
-    CHECK((from_standard_name_or_symbol("elemental caesium") == cs_elemental));
+    CHECK((from_standard_name("elemental caesium") == cs_elemental));
     CHECK(to_standard_name(cs_elemental, Nuclides::Standard::IUPAC) == "elemental caesium");
     CHECK(to_standard_name(cs_elemental, Nuclides::Standard::American) == "elemental cesium");
 
     // Particles
     constexpr EntityTag nu_e(nautilus::entity_tag::names::electron_neutrino);
     const std::string default_nu_e = "electron neutrino";
-    CHECK((from_standard_name_or_symbol(default_nu_e) == nu_e));
+    CHECK((from_standard_name(default_nu_e) == nu_e));
     CHECK(to_standard_name(nu_e) == default_nu_e);
     CHECK(to_standard_name(nu_e, Particles::Standard::PDG) == default_nu_e);
     CHECK(to_standard_name(nu_e, Particles::Standard::alternate) == default_nu_e);
@@ -96,32 +95,32 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
     constexpr EntityTag aL0(nautilus::entity_tag::names::neutral_lambda_antibaryon);
     const std::string default_aL0 = "neutral lambda antibaryon";
     const std::string alternate_aL0 = "antiparticle of the neutral lambda baryon";
-    CHECK((from_standard_name_or_symbol(default_aL0) == aL0));
-    CHECK((from_standard_name_or_symbol(alternate_aL0) == aL0));
+    CHECK((from_standard_name(default_aL0) == aL0));
+    CHECK((from_standard_name(alternate_aL0) == aL0));
     CHECK(to_standard_name(aL0) == default_aL0);
     CHECK(to_standard_name(aL0, Particles::Standard::PDG) == default_aL0);
     CHECK(to_standard_name(aL0, Particles::Standard::alternate) == alternate_aL0);
 
     // Distinction between hydrogen-1 (nuclide) and proton (particle)
     constexpr EntityTag proton(nautilus::entity_tag::names::proton);
-    CHECK((from_standard_name_or_symbol("proton") == proton));
+    CHECK((from_standard_name("proton") == proton));
     CHECK(to_standard_name(proton) == "proton");
     constexpr EntityTag h1(1, 1);
-    CHECK((from_standard_name_or_symbol("hydrogen-1") == h1));
+    CHECK((from_standard_name("hydrogen-1") == h1));
     CHECK(to_standard_name(h1) == "hydrogen-1");
 
     // First and last (verify bounds of indexing)
     // H-1 already done above
     constexpr EntityTag og294(118, 294);
-    CHECK((from_standard_name_or_symbol("oganesson-294") == og294));
+    CHECK((from_standard_name("oganesson-294") == og294));
     CHECK(to_standard_name(og294) == "oganesson-294");
     constexpr EntityTag g(nautilus::entity_tag::names::photon);
-    CHECK((from_standard_name_or_symbol("photon") == g));
+    CHECK((from_standard_name("photon") == g));
     CHECK(to_standard_name(g) == "photon");
     constexpr EntityTag aOm_plus(nautilus::entity_tag::names::positive_omega_antibaryon);
-    CHECK((from_standard_name_or_symbol("positive omega antibaryon") == aOm_plus));
-    CHECK((from_standard_name_or_symbol("positive omega antibaryon") == aOm_plus));
-    CHECK((from_standard_name_or_symbol("antiparticle of the negative omega baryon") == aOm_plus));
+    CHECK((from_standard_name("positive omega antibaryon") == aOm_plus));
+    CHECK((from_standard_name("positive omega antibaryon") == aOm_plus));
+    CHECK((from_standard_name("antiparticle of the negative omega baryon") == aOm_plus));
     CHECK(to_standard_name(aOm_plus) == "positive omega antibaryon");
     CHECK(
         to_standard_name(aOm_plus, Particles::Standard::PDG) == "positive omega antibaryon");
@@ -142,13 +141,13 @@ TEST_CASE("format: long standard name", "[entity_tag][format][standard name]")
 
     // Bad input
     constexpr EntityTag unknown(EntityTag::unknown);
-    CHECK((from_standard_name_or_symbol("BadInput") == unknown));         // garbage
-    CHECK((from_standard_name_or_symbol("elemental ") == unknown));       // incomplete
-    CHECK((from_standard_name_or_symbol("carbon elemental") == unknown)); // should be "elemental carbon"
-    CHECK((from_standard_name_or_symbol("elemental steel") == unknown));  // steel isn't a nuclide
-    CHECK((from_standard_name_or_symbol("brass-") == unknown));           // brass isn't a nuclide
-    CHECK((from_standard_name_or_symbol("nickel-") == unknown));          // incomplete
-    CHECK((from_standard_name_or_symbol("carbon-12z1") == unknown));      // invalid metastable indicator
+    CHECK((from_standard_name("BadInput") == unknown));         // garbage
+    CHECK((from_standard_name("elemental ") == unknown));       // incomplete
+    CHECK((from_standard_name("carbon elemental") == unknown)); // should be "elemental carbon"
+    CHECK((from_standard_name("elemental steel") == unknown));  // steel isn't a nuclide
+    CHECK((from_standard_name("brass-") == unknown));           // brass isn't a nuclide
+    CHECK((from_standard_name("nickel-") == unknown));          // incomplete
+    CHECK((from_standard_name("carbon-12z1") == unknown));      // invalid metastable indicator
 
     // "Bad" tag
     CHECK(to_standard_name(EntityTag(0, 0)) == "unknown"); // Z = 0
@@ -162,78 +161,77 @@ TEST_CASE("format: standard symbol", "[entity_tag][format][standard name]")
 {
     using nautilus::entity_tag::EntityTag;
     using nautilus::entity_tag::from_standard_symbol;
-    using nautilus::entity_tag::from_standard_name_or_symbol;
     using nautilus::entity_tag::to_standard_symbol;
     using nautilus::entity_tag::names::Nuclides;
     using nautilus::entity_tag::names::Particles;
 
     // "Normal" names with optional cases
     constexpr EntityTag co59g(27, 59);
-    CHECK((from_standard_name_or_symbol("Co-59") == co59g));
-    CHECK((from_standard_name_or_symbol("Co-59g") == co59g));
+    CHECK((from_standard_symbol("Co-59") == co59g));
+    CHECK((from_standard_symbol("Co-59g") == co59g));
     CHECK(to_standard_symbol(co59g) == "Co-59");
 
     constexpr EntityTag ta180m1(73, 180, 1);
-    CHECK((from_standard_name_or_symbol("Ta-180m") == ta180m1));
-    CHECK((from_standard_name_or_symbol("Ta-180m1") == ta180m1));
+    CHECK((from_standard_symbol("Ta-180m") == ta180m1));
+    CHECK((from_standard_symbol("Ta-180m1") == ta180m1));
     CHECK(to_standard_symbol(ta180m1) == "Ta-180m1");
 
     constexpr EntityTag k38m2(19, 38, 2);
-    CHECK((from_standard_name_or_symbol("K-38m2") == k38m2));
+    CHECK((from_standard_symbol("K-38m2") == k38m2));
     CHECK(to_standard_symbol(k38m2) == "K-38m2");
 
     // standard symbol does not have alternate spellings
 
     // Elementals
     constexpr EntityTag c_elemental(6);
-    CHECK((from_standard_name_or_symbol("C") == c_elemental));
+    CHECK((from_standard_symbol("C") == c_elemental));
     CHECK(to_standard_symbol(c_elemental) == "C");
     constexpr EntityTag cs_elemental(55);
-    CHECK((from_standard_name_or_symbol("Cs") == cs_elemental));
+    CHECK((from_standard_symbol("Cs") == cs_elemental));
     CHECK(to_standard_symbol(cs_elemental) == "Cs");
 
     // Particles
     constexpr EntityTag nu_e(nautilus::entity_tag::names::electron_neutrino);
     const std::string default_nu_e = "\u03BD\u2091";
-    CHECK((from_standard_name_or_symbol(default_nu_e) == nu_e));
+    CHECK((from_standard_symbol(default_nu_e) == nu_e));
     CHECK(to_standard_symbol(nu_e) == default_nu_e);
 
     constexpr EntityTag aL0(nautilus::entity_tag::names::neutral_lambda_antibaryon);
     const std::string default_aL0 = "\u039B\u0304\u2070";
-    CHECK((from_standard_name_or_symbol(default_aL0) == aL0));
+    CHECK((from_standard_symbol(default_aL0) == aL0));
     CHECK(to_standard_symbol(aL0) == default_aL0);
 
     // Distinction between hydrogen-1 (nuclide) and proton (particle)
     constexpr EntityTag proton(nautilus::entity_tag::names::proton);
-    CHECK((from_standard_name_or_symbol("p") == proton));
+    CHECK((from_standard_symbol("p") == proton));
     CHECK(to_standard_symbol(proton) == "p");
     constexpr EntityTag h1(1, 1);
-    CHECK((from_standard_name_or_symbol("H-1") == h1));
+    CHECK((from_standard_symbol("H-1") == h1));
     CHECK(to_standard_symbol(h1) == "H-1");
 
     // Distinction between similar:
     // -- p (proton) and P (phosphorus elemental); proton already checked above
     constexpr EntityTag p_elemental(15);
-    CHECK((from_standard_name_or_symbol("P") == p_elemental));
+    CHECK((from_standard_symbol("P") == p_elemental));
     CHECK(to_standard_symbol(p_elemental) == "P");
     // -- n (neutron) and N (nitrogen elemental)
     constexpr EntityTag neutron(nautilus::entity_tag::names::neutron);
     constexpr EntityTag n_elemental(7);
-    CHECK((from_standard_name_or_symbol("n") == neutron));
+    CHECK((from_standard_symbol("n") == neutron));
     CHECK(to_standard_symbol(neutron) == "n");
-    CHECK((from_standard_name_or_symbol("N") == n_elemental));
+    CHECK((from_standard_symbol("N") == n_elemental));
     CHECK(to_standard_symbol(n_elemental) == "N");
 
     // First and last (verify bounds of indexing)
     // H-1 already done above
     constexpr EntityTag og294(118, 294);
-    CHECK((from_standard_name_or_symbol("Og-294") == og294));
+    CHECK((from_standard_symbol("Og-294") == og294));
     CHECK(to_standard_symbol(og294) == "Og-294");
     constexpr EntityTag g(nautilus::entity_tag::names::photon);
-    CHECK((from_standard_name_or_symbol("\u03B3") == g));
+    CHECK((from_standard_symbol("\u03B3") == g));
     CHECK(to_standard_symbol(g) == "\u03B3");
     constexpr EntityTag aOm_plus(nautilus::entity_tag::names::positive_omega_antibaryon);
-    CHECK((from_standard_name_or_symbol("\u03A9\u0304\u207A") == aOm_plus));
+    CHECK((from_standard_symbol("\u03A9\u0304\u207A") == aOm_plus));
     CHECK(to_standard_symbol(aOm_plus) == "\u03A9\u0304\u207A");
 
     // User tag
@@ -247,9 +245,9 @@ TEST_CASE("format: standard symbol", "[entity_tag][format][standard name]")
     // Bad input
     constexpr EntityTag unknown(EntityTag::unknown);
     CHECK((from_standard_symbol("BadInput") == unknown)); // garbage
-    CHECK((from_standard_name_or_symbol("Xx-") == unknown));            // Xx isn't a nuclide
-    CHECK((from_standard_name_or_symbol("Ni-") == unknown));            // incomplete
-    CHECK((from_standard_name_or_symbol("Au-197q1") == unknown));       // invalid metastable state indicator
+    CHECK((from_standard_symbol("Xx-") == unknown));            // Xx isn't a nuclide
+    CHECK((from_standard_symbol("Ni-") == unknown));            // incomplete
+    CHECK((from_standard_symbol("Au-197q1") == unknown));       // invalid metastable state indicator
 
     // "Bad" tag
     CHECK(to_standard_symbol(EntityTag(0, 0)) == "unknown"); // Z = 0
