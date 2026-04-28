596b9186de0ef43dcbfd0a967741d02fb9215a22
Brendan K. Krueger <bkkrueger@gmail.com>
2026-04-28 16:24:54 -0600

    Expand explanatory comment.  Add an assertion to ensure that the user doesn't try to have more than 64 tests, because then this framework fails.

--- nautilus/util/gpu_check.hpp
+++ nautilus/util/gpu_check.hpp
@@ -9,8 +9,23 @@
 // This machinery will allow up to 64 calls to GPU_CHECK between init and final.  It reserves the
 // names rg, rc, ectr, and ES.  Calls to init and final MUST be properly paired, and only one such
 // pair can exist in a scope (consider using Catch2's SECTION to contain scopes).
+//
+// The fundamental concept is that GPU kernels and the `CHECK` macro don't play nice right now
+// (hopefully Catch2 will provide better support for GPUs in the future).  This machinery will
+// track errors by setting bits on an unsigned integer, then copy the results back to the CPU and
+// check that the overall flag is correct on the CPU.
+//
+// By having each test activate a different bit, this is limited to only 64 tests per scope.  But
+// it means that there is information that (with a little interpretation) identifies _which_ tests
+// failed on the GPU.
+//
+// By requiring that the results are default-fail, we can distinguish between a kernel that passed
+// all of its tests and a kernel that silently crashed early.  The downside is that the user must
+// pass the number of tests to GPU_CHECK_FINAL in order to verify that the correct number of tests
+// ran.
 constexpr uint64_t error_code(const uint64_t num_tests)
 {
+    assert(num_tests <= 64);
     uint64_t code = 0;
     for (uint64_t i = 0; i < num_tests; ++i) {
         code += 1 << i;
